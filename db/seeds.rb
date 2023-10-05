f0104fadb62e90c64ecc92ef189672e6ec3fd8a3
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-05 17:17:26 -0300

    Seed passengers and bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 Airport.destroy_all
 Flight.destroy_all
+Passenger.destroy_all
+Booking.destroy_all
 
 10.times do
   Airport.create(code: Array('A'..'Z').sample(3).join)
@@ -16,4 +18,16 @@ end
 10.times do
   interval = rand(0..23).hours + rand(0..59).minutes + rand(0..59).seconds
   Flight.create(start: DateTime.now + rand(-365..365), duration: interval, departure_airport_id: rand(1..10), arrival_airport_id: rand(1..10), seats: rand(1..80))
+end
+
+10.times do
+  name = Array('a'..'z').sample(8).join
+  email = "#{name}@#{Array('a'..'z').sample(5).join}.com"
+  Passenger.create(name: name, email: email)
+end
+
+5.times do
+  n1 = Array(1..10).sample(1).first
+  n2 = Array(1..10).sample(1).first
+  Flight.find(n1).passengers << Passenger.find(n2)
 end
