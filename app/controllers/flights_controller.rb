0a064aeb179b65dc623fae26a5d77648ff1acca9
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-04 23:38:33 -0300

    Show search results as a list of divs

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    @flights = Flight.where(departure_airport_id: params[:departure_airport_id])
+                      .and(Flight.where(arrival_airport_id: params[:arrival_airport_id]))
+                      .select { |flight| flight.start.to_date.strftime('%d/%m/%Y') == params[:start] }
   end
 end
