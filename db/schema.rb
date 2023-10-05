193b07875e1551c19f8441fc9ed68a84542268c9
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-05 13:27:44 -0300

    Create Passenger model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_011857) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_05_162147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_011857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "seats", null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
