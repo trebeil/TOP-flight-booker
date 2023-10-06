bc7f8e968f5e555301a1e018c333c37ac3b4d847
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-06 17:13:51 -0300

    Create create and show actions in the bookings controller and the show view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,5 +7,23 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      redirect_to @booking, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @flight = Flight.find(@booking.flight_id)
+    @passengers = @booking.passengers
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
