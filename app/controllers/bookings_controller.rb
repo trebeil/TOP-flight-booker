0306914e69564235d5acea7f01a84299d0001d4e
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-06 13:26:48 -0300

    Create method and view for bookings#new with nested form for passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,11 @@
 class BookingsController < ApplicationController
   def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new(flight_id: params[:flight_id])
+
+    params[:passengers].to_i.times { @booking.passengers.build }
+  end
+
+  def create
   end
 end
