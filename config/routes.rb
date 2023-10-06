563cc2f097198506bb8df10f193435962e83a512
Daniel Liebert <daniel.liebert@gmail.com>
2023-10-06 13:25:21 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookings/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "flights#index"
+  resources :flights
+  resources :bookings
 end
